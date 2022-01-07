42663e4de2d12089df8ff9a492a3238c1023bbd3
Wojciech Kawa <kawa.wojciech@outlook.com>
2022-01-07 19:44:50 +0100

    bump to laravel 8 and TailwindCSS 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 module.exports = {
-  purge: [
-      './resources/**/*.blade.php',
-      './resources/**/*.js',
-      './resources/**/*.vue',
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
   ],
-  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
